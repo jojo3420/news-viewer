64c363200415510f3b68355390f304312f4940db
dev_pjh-at-670977422354 <jjjhhhvvv@gmail.com>
2020-02-13 17:38:16 +0900

    first commit2

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import NewsList from './components/NewsList';
+
+function App() {
+	return (
+		<div className="App">
+			<h2>News-views</h2>
+			<NewsList />
+		</div>
+	);
+}
+
+export default App;
