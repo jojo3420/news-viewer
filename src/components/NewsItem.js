ec984b0513c5e06861e228941ed41e798f2bfd53
dev_pjh-at-670977422354 <jjjhhhvvv@gmail.com>
2020-02-13 17:37:50 +0900

    first commit

---
+++ src/components/NewsItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function NewsItem(props) {
+  return (
+    <div></div>
+  );
+}
+
+export default NewsItem;
---
+++ src/components/NewsList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import NewsItem from './NewsItem';
+
+function NewsList(props) {
+	return <div></div>;
+}
+
+export default NewsList;
